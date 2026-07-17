7263fb446fc09cb6584ff8e9ae60f90f31d7f53b
Dav Trần <davidtuber710@gmail.com>
2026-07-17 11:59:23 +0700

    up base

--- src/crawler/base.py
+++ src/crawler/base.py
@@ -1,1 +1,19 @@
+from playwright.async_api import async_playwright
+import asyncio
 
+
+class BaseCrawler:
+
+    async def start(self):
+
+        self.playwright = await async_playwright().start()
+
+        self.browser = await self.playwright.chromium.launch(
+            headless=True
+        )
+
+    async def stop(self):
+
+        await self.browser.close()
+
+        await self.playwright.stop()
